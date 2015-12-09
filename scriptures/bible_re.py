6f0b880d7d271d4992be39f874a303540235b569
Jeff Melton <jeff@themeltonplantation.com>
2015-12-09 15:07:23 -0600

    Fixed regex to match different input formats
    -	now matches "book chapter:verse" and "book chapter.verse"
    -	all tests passing

--- scriptures/bible_re.py
+++ scriptures/bible_re.py
@@ -131,9 +131,9 @@ book_re = re.compile(book_re_string, re.IGNORECASE | re.UNICODE)
 scripture_re = re.compile(
     r'\b(?P<BookTitle>%s)\s*'
     '(?P<ChapterNumber>\d{1,3})'
-    '(?:\s*:\s*(?P<VerseNumber>\d{1,3}))?'
+    '(?:\s*[:.]\s*(?P<VerseNumber>\d{1,3}))?'
     '(?:\s*[-\u2013\u2014]\s*'
-    '(?P<EndChapterNumber>\d{1,3}(?=\s*:\s*))?'
-    '(?:\s*:\s*)?'
+    '(?P<EndChapterNumber>\d{1,3}(?=\s*[:.]\s*))?'
+    '(?:\s*[:.]\s*)?'
     '(?P<EndVerseNumber>\d{1,3})?'
     ')?' % (book_re_string,), re.IGNORECASE | re.UNICODE)
