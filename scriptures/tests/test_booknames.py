8466819c25eb0fccf8de1e13ec7d44b4e495a294
Jeff Melton <jeff@themeltonplantation.com>
2015-12-11 09:37:40 -0600

    Revert "Merge branch 'develop'"
    Rolling back broken changes
    
    This reverts commit 2511ad1bb4497333b0a706d6222dcaa7d951a720, reversing
    changes made to 74a033b0fd1e946b0d64fb0ba191f90e6d508f6e.

--- scriptures/tests/test_booknames.py
+++ scriptures/tests/test_booknames.py
@@ -49,56 +49,56 @@ class TestBookNames(unittest.TestCase):
         self.assertEqual(f('ruth 1:1'), 'Ruth 1.1')
 
     def test_i_samuel(self):
-        self.assertEqual(f('1 samuel 1:1'), '1 Samuel 1.1')
-        self.assertEqual(f('1 samuel 1:1'), '1 Samuel 1.1')
-        self.assertEqual(f('1 sam 1:1'), '1 Samuel 1.1')
-        self.assertEqual(f('1 sam 1:1'), '1 Samuel 1.1')
-        self.assertEqual(f('1sam 1:1'), '1 Samuel 1.1')
+        self.assertEqual(f('I samuel 1:1'), 'I Samuel 1.1')
+        self.assertEqual(f('1 samuel 1:1'), 'I Samuel 1.1')
+        self.assertEqual(f('I sam 1:1'), 'I Samuel 1.1')
+        self.assertEqual(f('1 sam 1:1'), 'I Samuel 1.1')
+        self.assertEqual(f('1sam 1:1'), 'I Samuel 1.1')
 
     def test_ii_samuel(self):
-        self.assertEqual(f('2 samuel 1:1'), '2 Samuel 1.1')
-        self.assertEqual(f('2 samuel 1:1'), '2 Samuel 1.1')
-        self.assertEqual(f('2 sam 1:1'), '2 Samuel 1.1')
-        self.assertEqual(f('2 sam 1:1'), '2 Samuel 1.1')
-        self.assertEqual(f('2sam 1:1'), '2 Samuel 1.1')
+        self.assertEqual(f('II samuel 1:1'), 'II Samuel 1.1')
+        self.assertEqual(f('2 samuel 1:1'), 'II Samuel 1.1')
+        self.assertEqual(f('II sam 1:1'), 'II Samuel 1.1')
+        self.assertEqual(f('2 sam 1:1'), 'II Samuel 1.1')
+        self.assertEqual(f('2sam 1:1'), 'II Samuel 1.1')
 
     def test_i_kings(self):
-        self.assertEqual(f('1 kings 1:1'), '1 Kings 1.1')
-        self.assertEqual(f('1 kings 1:1'), '1 Kings 1.1')
-        self.assertEqual(f('1 kgs 1:1'), '1 Kings 1.1')
-        self.assertEqual(f('1 kgs 1:1'), '1 Kings 1.1')
-        self.assertEqual(f('1kgs 1:1'), '1 Kings 1.1')
+        self.assertEqual(f('I kings 1:1'), 'I Kings 1.1')
+        self.assertEqual(f('1 kings 1:1'), 'I Kings 1.1')
+        self.assertEqual(f('I kgs 1:1'), 'I Kings 1.1')
+        self.assertEqual(f('1 kgs 1:1'), 'I Kings 1.1')
+        self.assertEqual(f('1kgs 1:1'), 'I Kings 1.1')
 
     def test_ii_kings(self):
-        self.assertEqual(f('2 kings 1:1'), '2 Kings 1.1')
-        self.assertEqual(f('2 kings 1:1'), '2 Kings 1.1')
-        self.assertEqual(f('2 kgs 1:1'), '2 Kings 1.1')
-        self.assertEqual(f('2 kgs 1:1'), '2 Kings 1.1')
-        self.assertEqual(f('2kgs 1:1'), '2 Kings 1.1')
+        self.assertEqual(f('II kings 1:1'), 'II Kings 1.1')
+        self.assertEqual(f('2 kings 1:1'), 'II Kings 1.1')
+        self.assertEqual(f('II kgs 1:1'), 'II Kings 1.1')
+        self.assertEqual(f('2 kgs 1:1'), 'II Kings 1.1')
+        self.assertEqual(f('2kgs 1:1'), 'II Kings 1.1')
 
     def test_i_chronicles(self):
-        self.assertEqual(f('1 chronicles 1:1'), '1 Chronicles 1.1')
-        self.assertEqual(f('1 chronicles 1:1'), '1 Chronicles 1.1')
+        self.assertEqual(f('I chronicles 1:1'), 'I Chronicles 1.1')
+        self.assertEqual(f('1 chronicles 1:1'), 'I Chronicles 1.1')
 
-        self.assertEqual(f('1 chr 1:1'), '1 Chronicles 1.1')
-        self.assertEqual(f('1 chro 1:1'), '1 Chronicles 1.1')
-        self.assertEqual(f('1 chron 1:1'), '1 Chronicles 1.1')
+        self.assertEqual(f('I chr 1:1'), 'I Chronicles 1.1')
+        self.assertEqual(f('I chro 1:1'), 'I Chronicles 1.1')
+        self.assertEqual(f('I chron 1:1'), 'I Chronicles 1.1')
 
-        self.assertEqual(f('1 chr 1:1'), '1 Chronicles 1.1')
-        self.assertEqual(f('1 chro 1:1'), '1 Chronicles 1.1')
-        self.assertEqual(f('1 chron 1:1'), '1 Chronicles 1.1')
+        self.assertEqual(f('1 chr 1:1'), 'I Chronicles 1.1')
+        self.assertEqual(f('1 chro 1:1'), 'I Chronicles 1.1')
+        self.assertEqual(f('1 chron 1:1'), 'I Chronicles 1.1')
 
     def test_ii_chronicles(self):
-        self.assertEqual(f('2 chronicles 1:1'), '2 Chronicles 1.1')
-        self.assertEqual(f('2 chronicles 1:1'), '2 Chronicles 1.1')
+        self.assertEqual(f('II chronicles 1:1'), 'II Chronicles 1.1')
+        self.assertEqual(f('2 chronicles 1:1'), 'II Chronicles 1.1')
 
-        self.assertEqual(f('2 chr 1:1'), '2 Chronicles 1.1')
-        self.assertEqual(f('2 chro 1:1'), '2 Chronicles 1.1')
-        self.assertEqual(f('2 chron 1:1'), '2 Chronicles 1.1')
+        self.assertEqual(f('II chr 1:1'), 'II Chronicles 1.1')
+        self.assertEqual(f('II chro 1:1'), 'II Chronicles 1.1')
+        self.assertEqual(f('II chron 1:1'), 'II Chronicles 1.1')
 
-        self.assertEqual(f('2 chr 1:1'), '2 Chronicles 1.1')
-        self.assertEqual(f('2 chro 1:1'), '2 Chronicles 1.1')
-        self.assertEqual(f('2 chron 1:1'), '2 Chronicles 1.1')
+        self.assertEqual(f('2 chr 1:1'), 'II Chronicles 1.1')
+        self.assertEqual(f('2 chro 1:1'), 'II Chronicles 1.1')
+        self.assertEqual(f('2 chron 1:1'), 'II Chronicles 1.1')
 
     def test_ezra(self):
         self.assertEqual(f('ezra 1:1'), 'Ezra 1.1')
@@ -222,26 +222,26 @@ class TestBookNames(unittest.TestCase):
         self.assertEqual(f('rom 1:1'), 'Romans 1.1')
 
     def test_i_corinthians(self):
-        self.assertEqual(f('1 corinthians 1:1'), '1 Corinthians 1.1')
-        self.assertEqual(f('1 cor 1:1'), '1 Corinthians 1.1')
-        self.assertEqual(f('1 corinthians 1:1'), '1 Corinthians 1.1')
-        self.assertEqual(f('1 cor 1:1'), '1 Corinthians 1.1')
+        self.assertEqual(f('I corinthians 1:1'), 'I Corinthians 1.1')
+        self.assertEqual(f('I cor 1:1'), 'I Corinthians 1.1')
+        self.assertEqual(f('1 corinthians 1:1'), 'I Corinthians 1.1')
+        self.assertEqual(f('1 cor 1:1'), 'I Corinthians 1.1')
 
-        self.assertEqual(f('1corinthians 1:1'), '1 Corinthians 1.1')
-        self.assertEqual(f('1cor 1:1'), '1 Corinthians 1.1')
-        self.assertEqual(f('1corinthians 1:1'), '1 Corinthians 1.1')
-        self.assertEqual(f('1cor 1:1'), '1 Corinthians 1.1')
+        self.assertEqual(f('Icorinthians 1:1'), 'I Corinthians 1.1')
+        self.assertEqual(f('Icor 1:1'), 'I Corinthians 1.1')
+        self.assertEqual(f('1corinthians 1:1'), 'I Corinthians 1.1')
+        self.assertEqual(f('1cor 1:1'), 'I Corinthians 1.1')
 
     def test_ii_corinthians(self):
-        self.assertEqual(f('2 corinthians 1:1'), '2 Corinthians 1.1')
-        self.assertEqual(f('2 cor 1:1'), '2 Corinthians 1.1')
-        self.assertEqual(f('2 corinthians 1:1'), '2 Corinthians 1.1')
-        self.assertEqual(f('2 cor 1:1'), '2 Corinthians 1.1')
+        self.assertEqual(f('II corinthians 1:1'), 'II Corinthians 1.1')
+        self.assertEqual(f('II cor 1:1'), 'II Corinthians 1.1')
+        self.assertEqual(f('2 corinthians 1:1'), 'II Corinthians 1.1')
+        self.assertEqual(f('2 cor 1:1'), 'II Corinthians 1.1')
 
-        self.assertEqual(f('2corinthians 1:1'), '2 Corinthians 1.1')
-        self.assertEqual(f('2cor 1:1'), '2 Corinthians 1.1')
-        self.assertEqual(f('2corinthians 1:1'), '2 Corinthians 1.1')
-        self.assertEqual(f('2cor 1:1'), '2 Corinthians 1.1')
+        self.assertEqual(f('IIcorinthians 1:1'), 'II Corinthians 1.1')
+        self.assertEqual(f('IIcor 1:1'), 'II Corinthians 1.1')
+        self.assertEqual(f('2corinthians 1:1'), 'II Corinthians 1.1')
+        self.assertEqual(f('2cor 1:1'), 'II Corinthians 1.1')
 
     def test_galatians(self):
         self.assertEqual(f('galatians 1:1'), 'Galatians 1.1')
@@ -260,48 +260,48 @@ class TestBookNames(unittest.TestCase):
         self.assertEqual(f('col 1:1'), 'Colossians 1.1')
 
     def test_i_thessalonians(self):
-        self.assertEqual(f('1 thessalonians 1:1'), '1 Thessalonians 1.1')
-        self.assertEqual(f('1 thess 1:1'), '1 Thessalonians 1.1')
-        self.assertEqual(f('1 thessalonians 1:1'), '1 Thessalonians 1.1')
-        self.assertEqual(f('1 thess 1:1'), '1 Thessalonians 1.1')
+        self.assertEqual(f('I thessalonians 1:1'), 'I Thessalonians 1.1')
+        self.assertEqual(f('I thess 1:1'), 'I Thessalonians 1.1')
+        self.assertEqual(f('1 thessalonians 1:1'), 'I Thessalonians 1.1')
+        self.assertEqual(f('1 thess 1:1'), 'I Thessalonians 1.1')
 
-        self.assertEqual(f('1thessalonians 1:1'), '1 Thessalonians 1.1')
-        self.assertEqual(f('1thess 1:1'), '1 Thessalonians 1.1')
-        self.assertEqual(f('1thessalonians 1:1'), '1 Thessalonians 1.1')
-        self.assertEqual(f('1thess 1:1'), '1 Thessalonians 1.1')
+        self.assertEqual(f('Ithessalonians 1:1'), 'I Thessalonians 1.1')
+        self.assertEqual(f('Ithess 1:1'), 'I Thessalonians 1.1')
+        self.assertEqual(f('1thessalonians 1:1'), 'I Thessalonians 1.1')
+        self.assertEqual(f('1thess 1:1'), 'I Thessalonians 1.1')
 
     def test_ii_thessalonians(self):
-        self.assertEqual(f('2 thessalonians 1:1'), '2 Thessalonians 1.1')
-        self.assertEqual(f('2 thess 1:1'), '2 Thessalonians 1.1')
-        self.assertEqual(f('2 thessalonians 1:1'), '2 Thessalonians 1.1')
-        self.assertEqual(f('2 thess 1:1'), '2 Thessalonians 1.1')
+        self.assertEqual(f('II thessalonians 1:1'), 'II Thessalonians 1.1')
+        self.assertEqual(f('II thess 1:1'), 'II Thessalonians 1.1')
+        self.assertEqual(f('2 thessalonians 1:1'), 'II Thessalonians 1.1')
+        self.assertEqual(f('2 thess 1:1'), 'II Thessalonians 1.1')
 
-        self.assertEqual(f('2thessalonians 1:1'), '2 Thessalonians 1.1')
-        self.assertEqual(f('2thess 1:1'), '2 Thessalonians 1.1')
-        self.assertEqual(f('2thessalonians 1:1'), '2 Thessalonians 1.1')
-        self.assertEqual(f('2thess 1:1'), '2 Thessalonians 1.1')
+        self.assertEqual(f('IIthessalonians 1:1'), 'II Thessalonians 1.1')
+        self.assertEqual(f('IIthess 1:1'), 'II Thessalonians 1.1')
+        self.assertEqual(f('2thessalonians 1:1'), 'II Thessalonians 1.1')
+        self.assertEqual(f('2thess 1:1'), 'II Thessalonians 1.1')
 
     def test_i_timothy(self):
-        self.assertEqual(f('1 timothy 1:1'), '1 Timothy 1.1')
-        self.assertEqual(f('1 tim 1:1'), '1 Timothy 1.1')
-        self.assertEqual(f('1 timothy 1:1'), '1 Timothy 1.1')
-        self.assertEqual(f('1 tim 1:1'), '1 Timothy 1.1')
+        self.assertEqual(f('I timothy 1:1'), 'I Timothy 1.1')
+        self.assertEqual(f('I tim 1:1'), 'I Timothy 1.1')
+        self.assertEqual(f('1 timothy 1:1'), 'I Timothy 1.1')
+        self.assertEqual(f('1 tim 1:1'), 'I Timothy 1.1')
 
-        self.assertEqual(f('1timothy 1:1'), '1 Timothy 1.1')
-        self.assertEqual(f('1tim 1:1'), '1 Timothy 1.1')
-        self.assertEqual(f('1timothy 1:1'), '1 Timothy 1.1')
-        self.assertEqual(f('1tim 1:1'), '1 Timothy 1.1')
+        self.assertEqual(f('Itimothy 1:1'), 'I Timothy 1.1')
+        self.assertEqual(f('Itim 1:1'), 'I Timothy 1.1')
+        self.assertEqual(f('1timothy 1:1'), 'I Timothy 1.1')
+        self.assertEqual(f('1tim 1:1'), 'I Timothy 1.1')
 
     def test_ii_timothy(self):
-        self.assertEqual(f('2 timothy 1:1'), '2 Timothy 1.1')
-        self.assertEqual(f('2 tim 1:1'), '2 Timothy 1.1')
-        self.assertEqual(f('2 timothy 1:1'), '2 Timothy 1.1')
-        self.assertEqual(f('2 tim 1:1'), '2 Timothy 1.1')
+        self.assertEqual(f('II timothy 1:1'), 'II Timothy 1.1')
+        self.assertEqual(f('II tim 1:1'), 'II Timothy 1.1')
+        self.assertEqual(f('2 timothy 1:1'), 'II Timothy 1.1')
+        self.assertEqual(f('2 tim 1:1'), 'II Timothy 1.1')
 
-        self.assertEqual(f('2timothy 1:1'), '2 Timothy 1.1')
-        self.assertEqual(f('2tim 1:1'), '2 Timothy 1.1')
-        self.assertEqual(f('2timothy 1:1'), '2 Timothy 1.1')
-        self.assertEqual(f('2tim 1:1'), '2 Timothy 1.1')
+        self.assertEqual(f('IItimothy 1:1'), 'II Timothy 1.1')
+        self.assertEqual(f('IItim 1:1'), 'II Timothy 1.1')
+        self.assertEqual(f('2timothy 1:1'), 'II Timothy 1.1')
+        self.assertEqual(f('2tim 1:1'), 'II Timothy 1.1')
 
     def test_titus(self):
         self.assertEqual(f('titus 1:1'), 'Titus 1.1')
@@ -322,47 +322,47 @@ class TestBookNames(unittest.TestCase):
         self.assertEqual(f('jas 1:1'), 'James 1.1')
 
     def test_i_peter(self):
-        self.assertEqual(f('1 peter 1:1'), '1 Peter 1.1')
-        self.assertEqual(f('1 pet 1:1'), '1 Peter 1.1')
-        self.assertEqual(f('1 peter 1:1'), '1 Peter 1.1')
-        self.assertEqual(f('1 pet 1:1'), '1 Peter 1.1')
+        self.assertEqual(f('I peter 1:1'), 'I Peter 1.1')
+        self.assertEqual(f('I pet 1:1'), 'I Peter 1.1')
+        self.assertEqual(f('1 peter 1:1'), 'I Peter 1.1')
+        self.assertEqual(f('1 pet 1:1'), 'I Peter 1.1')
 
-        self.assertEqual(f('1peter 1:1'), '1 Peter 1.1')
-        self.assertEqual(f('1pet 1:1'), '1 Peter 1.1')
-        self.assertEqual(f('1peter 1:1'), '1 Peter 1.1')
-        self.assertEqual(f('1pet 1:1'), '1 Peter 1.1')
+        self.assertEqual(f('Ipeter 1:1'), 'I Peter 1.1')
+        self.assertEqual(f('Ipet 1:1'), 'I Peter 1.1')
+        self.assertEqual(f('1peter 1:1'), 'I Peter 1.1')
+        self.assertEqual(f('1pet 1:1'), 'I Peter 1.1')
 
     def test_i_peter(self):
-        self.assertEqual(f('2 peter 1:1'), '2 Peter 1.1')
-        self.assertEqual(f('2 pet 1:1'), '2 Peter 1.1')
-        self.assertEqual(f('2 peter 1:1'), '2 Peter 1.1')
-        self.assertEqual(f('2 pet 1:1'), '2 Peter 1.1')
+        self.assertEqual(f('II peter 1:1'), 'II Peter 1.1')
+        self.assertEqual(f('II pet 1:1'), 'II Peter 1.1')
+        self.assertEqual(f('2 peter 1:1'), 'II Peter 1.1')
+        self.assertEqual(f('2 pet 1:1'), 'II Peter 1.1')
 
-        self.assertEqual(f('2peter 1:1'), '2 Peter 1.1')
-        self.assertEqual(f('2pet 1:1'), '2 Peter 1.1')
-        self.assertEqual(f('2peter 1:1'), '2 Peter 1.1')
-        self.assertEqual(f('2pet 1:1'), '2 Peter 1.1')
+        self.assertEqual(f('IIpeter 1:1'), 'II Peter 1.1')
+        self.assertEqual(f('IIpet 1:1'), 'II Peter 1.1')
+        self.assertEqual(f('2peter 1:1'), 'II Peter 1.1')
+        self.assertEqual(f('2pet 1:1'), 'II Peter 1.1')
 
     def test_i_john(self):
-        self.assertEqual(f('1 john 1:1'), '1 John 1.1')
-        self.assertEqual(f('1 john 1:1'), '1 John 1.1')
+        self.assertEqual(f('I john 1:1'), 'I John 1.1')
+        self.assertEqual(f('1 john 1:1'), 'I John 1.1')
 
-        self.assertEqual(f('1john 1:1'), '1 John 1.1')
-        self.assertEqual(f('1john 1:1'), '1 John 1.1')
+        self.assertEqual(f('Ijohn 1:1'), 'I John 1.1')
+        self.assertEqual(f('1john 1:1'), 'I John 1.1')
 
     def test_ii_John(self):
-        self.assertEqual(f('2 john 1:1'), '2 John 1')
-        self.assertEqual(f('2 john 1:1'), '2 John 1')
+        self.assertEqual(f('II john 1:1'), 'II John 1')
+        self.assertEqual(f('2 john 1:1'), 'II John 1')
 
-        self.assertEqual(f('2john 1:1'), '2 John 1')
-        self.assertEqual(f('2john 1:1'), '2 John 1')
+        self.assertEqual(f('IIjohn 1:1'), 'II John 1')
+        self.assertEqual(f('2john 1:1'), 'II John 1')
 
     def test_iii_john(self):
-        self.assertEqual(f('3 john 1:1'), '3 John 1')
-        self.assertEqual(f('3 john 1:1'), '3 John 1')
+        self.assertEqual(f('III john 1:1'), 'III John 1')
+        self.assertEqual(f('3 john 1:1'), 'III John 1')
 
-        self.assertEqual(f('3john 1:1'), '3 John 1')
-        self.assertEqual(f('3john 1:1'), '3 John 1')
+        self.assertEqual(f('IIIjohn 1:1'), 'III John 1')
+        self.assertEqual(f('3john 1:1'), 'III John 1')
 
     def test_jude(self):
         self.assertEqual(f('jude 1:1'), 'Jude 1')
